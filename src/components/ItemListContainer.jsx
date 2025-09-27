d1d6110be5f7394f7d4a0aa8e9516cff19b7d679
FacundoSuarez <facundosuarezc@gmail.com>
2025-09-27 14:06:04 -0300

    creo variable de estado con hook useState para poner allí las excursiones, uso el hook useEffect para cargar las excursiones mediante una promesa y para actualizar el DOM cuando se cumple la misma que me devuelve el array.

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,16 +1,33 @@
 import './ItemListContainer.css'
 import excursiones from '../../data/excursiones';
 import Excursion from './Excursion';
+import obtenerExcursiones from '../../data/MockAPIExcursiones';
+import { useState, useEffect } from 'react';
+
+let excursionesObtenidas = []; // array para traer excursiones
 
 export default function ItemListContainer(props) {
-  
+  const [excursionesObtenidas, setExcursionesObtenidas] = useState([]); //por defecto array excursiones vacío
+
+  useEffect ( () => {
+    //codigo para obtener las excursiones mediante promesa
+    obtenerExcursiones()
+    .then( (value) => {
+      setExcursionesObtenidas(value);  //caso exitoso, uso setter para asignar el valor obtenido
+    })
+    .catch( (error) => {
+      console.error ("Hubo un error al recuperar excursiones: " + error);  //caso error, muestro por consola
+    })
+  }, [excursionesObtenidas]) 
+   
+
   return (
     //texto de bienvenida primer renglón
     //abajo una card por cada excursion
     <div>
       <div className="bienvenida"> {props.mensaje}</div>      
       <div className="excursiones">
-        {excursiones.map(excursion => (
+        {excursionesObtenidas.map(excursion => (
           <Excursion key={excursion.codigoTour} {...excursion} />
         ))}
       </div>
